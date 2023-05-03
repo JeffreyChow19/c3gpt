e7a412a7f33a58549af367d9b438faab999ae5bd
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-05-03 14:24:08 +0700

    fix: return message

--- handlers/qna.js
+++ handlers/qna.js
@@ -8,7 +8,7 @@ async function handleCreateQnA(question, answer) {
 
   try {
     await qna.save();
-    return qna;
+    return JSON.stringify(qna.toObject());
   } catch (error) {
     throw new Error("An error occurred while saving the QnA document");
   }
@@ -20,9 +20,9 @@ async function handleGetQnAs() {
 
   try {
     const qnas = await QnA.find();
-    res.json(qnas);
+    return JSON.stringify(qnas);
   } catch (error) {
-    res.status(500).send("An error occurred while retrieving QnA documents");
+    throw new Error("An error occurred while retrieving QnA documents");
   }
 }
 
@@ -36,9 +36,9 @@ async function handleUpdateQnA(_id, question, answer) {
       { question, answer },
       { new: true }
     );
-    res.json(qna);
+    return JSON.stringify(qna);
   } catch (error) {
-    res.status(500).send("An error occurred while updating the QnA document");
+    throw new Error("An error occurred while updating the QnA document");
   }
 }
 
@@ -48,14 +48,54 @@ async function handleDeleteQnA(id) {
 
   try {
     const deletedQnA = await QnA.findByIdAndDelete(id);
-    res.send(deletedQnA);
+    return JSON.stringify(deletedQnA);
   } catch (error) {
-    res.status(500).send("An error occurred while deleting the QnA document");
+    throw new Error("An error occurred while deleting the QnA document");
   }
 }
 
 async function handleGetResponse(req, res) {
   const { question, method } = req.query;
+
+  // try {
+  //   const qnasJSON = await handleGetQnAs();
+  //   const qnas = JSON.parse(qnasJSON);
+  //   console.log(qnas);
+  //   res.json(qnas);
+  // } catch (error) {
+  //   res.status(500).send(error);
+  // }
+
+  // try {
+  //   let result = await handleCreateQnA(
+  //     "What is your name?",
+  //     "My name is C3GPT2."
+  //   );
+  //   console.log(result);
+  //   res.status(200).json(result);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  // try {
+  //   let result = await handleUpdateQnA(
+  //     "645205e1d94fc51a96ce6349",
+  //     "Siapa namamu",
+  //     "C3 bang"
+  //   );
+  //   console.log(result);
+  //   res.status(200).json(JSON.stringify(result));
+  // } catch (error) {
+  //   res.status(500).send(error);
+  // }
+
+  // try {
+  //   let result = await handleDeleteQnA("64520613d94fc51a96ce634b");
+  //   console.log(result);
+  //   res.status(200), json(result);
+  // } catch (error) {
+  //   res.status(500).send(error);
+  // }
 
   // BRANCHING
 }
